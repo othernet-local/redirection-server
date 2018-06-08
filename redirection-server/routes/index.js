722b23f59ea76b6a93efafbc65b1765e455e7e15
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2018-06-08 13:22:34 -0400

    redirection

--- redirection-server/routes/index.js
+++ redirection-server/routes/index.js
@@ -5,26 +5,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	host = req.host.split('.othernet')[0]
 	subs = host.split('.')
-	domain = "http://quagga.club/"
-	
-	subs.forEach(function(d){
-		domain += "?"+d
-	})
-	res.redirect(domain);
-
-
+	domain = "http://206.189.180.30/"
+	if (subs[0] == "home"){
+		res.render('index', {title: 'express'});
+	}else{
+		subs.forEach(function(d){
+			domain += "?"+d
+		})
+		res.redirect(domain);
+	}
 });
 router.get('/:edit', function(req, res, next) {
 	host = req.host.split('.othernet')[0]
 	subs = host.split('.')
-	domain = "http://quagga.club/"
+	domain = "http://206.189.180.30/"
 	
 	subs.forEach(function(d){
 		domain += "?"+d
 	})
 
 	if (req.params.edit == "edit"){
-		domain += "?edit"
+		domain += "/edit"
 	}
 
 	res.redirect(domain);
